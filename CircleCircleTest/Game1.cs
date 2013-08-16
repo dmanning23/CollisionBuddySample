d5be5b4e73324629cfe156b4c2bec2ce7d0fbf28
Daniel Manning <daniel.manning@demandmedia.com>
2013-08-16 17:40:54 -0500

    added escape key to quit

--- CircleCircleTest/Game1.cs
+++ CircleCircleTest/Game1.cs
@@ -91,8 +91,11 @@ namespace CircleCircleTest
 		protected override void Update(GameTime gameTime)
 		{
 			// Allows the game to exit
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+			if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) || 
+				Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
 				this.Exit();
+			}
 
 			//update the timer
 			_clock.Update(gameTime);
